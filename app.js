521b15576af25ba1587c58cab7cba95f38114e0a
Esther White <64324417+monacodelisa@users.noreply.github.com>
2023-04-24 18:34:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,11 +37,10 @@ function read() {
     letter.classList.add("expanded");
 
     const pass = prompt('password');
-    if (pass == '37fd4045') {
-        letter.innerHTML = `<div class="letter-text"><p>Мила Моли,</p> <p>Няма достатъчно думи с които да мога да изразя колко много те обичам и колко се гордея с теб. <p>Ти заемаш специално място в моето сърце от момента, в който се роди. Възхищавам се на твоята красота, твоите умения, и на твоята смелост.</p> <p></p>Всяка година те наблюдавах как растеш, как се развиваш и как превръщаш във великолепна млада жена. Имаш уникални таланти и способности, и това, което постигаш, е невероятно впечатляващо.</p>
-        <p> Искам да знаеш, че никога не се колебая да те подкрепям и да ти помагам да постигнеш всичко, което пожелаеш.</p>
-        <p>Ти си моята любима дъщеря, моето съкровище, обичам те to the moon and back. </p><p> באהבה
-        <br>мама Ести.</p>
+    if (pass == 'lorem') {
+        letter.innerHTML = `<div class="letter-text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.Nulla quis sem at nibh elementum 
+        imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta</p>
         </div>`
     }
   
